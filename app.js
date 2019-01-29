cbaf4b76d28ee1351998f41c1bbbf54f435ec105
wodzynski <macko5395@gmail.com>
2019-01-29 21:34:15 +0100

    Added register routes and form

---
+++ app.js
@@ -0,0 +1,48 @@
+const express               = require('express'),
+      app                   = express(),
+      mongoose              = require('mongoose'),
+      passport              = require('passport'),
+      bodyParser            = require('body-parser'),
+      User                  = require('./models/user'),
+      LocalStrategy         = require('passport-local'),
+      passportLocalMongoose = require('passport-local-mongoose');
+
+mongoose.connect('mongodb://localhost:27017/auth_demo_app', {useNewUrlParser: true});
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(require('express-session')({
+  secret: 'Tea is the best drink in the world',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// ROUTES
+
+app.get('/', (req, res) => res.render('home'));
+
+app.get('/secret', (req,res) => res.render('secret'));
+
+// AUTH ROUTES
+
+app.get('/register', (req, res) => res.render('register'));
+
+// handling user sign up
+app.post('/register', (req, res) => {
+  User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+    if(err) {
+      console.log(err);
+      return res.render('register');
+    } else {
+      passport.authenticate('local')(req, res, () => res.redirect('/secret'));
+    }
+  });
+});
+
+app.listen(3000, process.env.IP, () => console.log('The server has started!'));
